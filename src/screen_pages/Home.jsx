4ab75a115e469762358684f8507c68edc3bac742
adrian-g-schmidt <adrian.g.schmidt@gmail.com>
2025-02-10 21:07:38 +1100

    update spins

--- src/screen_pages/Home.jsx
+++ src/screen_pages/Home.jsx
@@ -1,9 +1,8 @@
 import { useState, useRef, useEffect } from "react";
 
 export default function Home({ handleNavigate }) {
-  const [currentVideo, setCurrentVideo] = useState("vin_spin.mp4");
+  const [currentVideo, setCurrentVideo] = useState("h_spin.mp4");
   const videoRef = useRef(null);
-
   useEffect(() => {
     if (videoRef.current) {
       videoRef.current.load();
@@ -18,7 +17,7 @@ export default function Home({ handleNavigate }) {
             className="-my-1 uppercase text-xl text-white hover:text-red-600 cursor-pointer flex items-center justify-center"
             onClick={() => handleNavigate("who")}
             onMouseEnter={() => setCurrentVideo("vin_spin.mp4")}
-            onMouseLeave={() => setCurrentVideo("vin_spin.mp4")}
+            onMouseLeave={() => setCurrentVideo("h_spin.mp4")}
           >
             1. Who?
           </button>
@@ -26,7 +25,7 @@ export default function Home({ handleNavigate }) {
             className="-my-1 uppercase text-xl text-white hover:text-red-600 cursor-pointer flex items-center justify-center"
             onClick={() => handleNavigate("what")}
             onMouseEnter={() => setCurrentVideo("cam_spin.mp4")}
-            onMouseLeave={() => setCurrentVideo("vin_spin.mp4")}
+            onMouseLeave={() => setCurrentVideo("h_spin.mp4")}
           >
             2. What?
           </button>
@@ -35,15 +34,15 @@ export default function Home({ handleNavigate }) {
         text-xl text-white hover:text-red-500 cursor-pointer flex items-center justify-center"
             onClick={() => handleNavigate("when")}
             onMouseEnter={() => setCurrentVideo("dionysis_spin.mp4")}
-            onMouseLeave={() => setCurrentVideo("vin_spin.mp4")}
+            onMouseLeave={() => setCurrentVideo("h_spin.mp4")}
           >
             3. When?
           </button>
           <button
             className="-my-1 uppercase  text-xl text-white hover:text-red-500 cursor-pointer flex items-center justify-center"
             onClick={() => handleNavigate("where")}
-            onMouseEnter={() => setCurrentVideo("vin_spin.mp4")}
-            onMouseLeave={() => setCurrentVideo("vin_spin.mp4")}
+            onMouseEnter={() => setCurrentVideo("earth_spin.mp4")}
+            onMouseLeave={() => setCurrentVideo("h_spin.mp4")}
           >
             4. Where?
           </button>
@@ -51,18 +50,18 @@ export default function Home({ handleNavigate }) {
             className="-my-1 uppercase  text-xl text-white hover:text-red-500 cursor-pointer flex items-center justify-center"
             onClick={() => handleNavigate("why")}
             onMouseEnter={() => setCurrentVideo("cash_spin.mp4")}
-            onMouseLeave={() => setCurrentVideo("vin_spin.mp4")}
+            onMouseLeave={() => setCurrentVideo("h_spin.mp4")}
           >
             5. Why?
           </button>
-          <button
+          {/* <button
             className="-my-1 uppercase  text-xl text-white hover:text-red-500 cursor-pointer flex items-center justify-center"
             onClick={() => handleNavigate("how")}
             onMouseEnter={() => setCurrentVideo("vin_spin.mp4")}
             onMouseLeave={() => setCurrentVideo("vin_spin.mp4")}
           >
             6. How?
-          </button>
+          </button> */}
         </div>
         <video ref={videoRef} autoPlay loop muted className="z-0">
           <source src={`./home_videos/${currentVideo}`} type="video/mp4" />
